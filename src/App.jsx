b68027824583b99de6db22a6f9a1a50bb4c59aff
khushi batra <khushi@Khushis-MacBook-Pro.local>
2025-04-10 21:00:43 +0530

    Changes1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import styles from'./App.module.css';
 import { Hero } from './components/Hero/Hero';
-import { Navbar } from './components/Navbar/navbar';
+import { Navbar } from './components/Navbar/Navbar';
 import { About } from './components/About/About';
 import {Education} from "./components/EDUCATION/Education.jsx";
 import {Projects} from "./components/Projects/Projects"
 import { Contact} from "./components/Contact/Contact.jsx"
+
 
 function App() {
  
